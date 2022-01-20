6195db0a0326ffc67f626701195bbf95b2c730d5
Nathan DEMURU <blakei.57@icloud.com>
2022-01-20 22:54:08 +0100

    Initial commit

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+function TodoForm(props) {
+    const [input, setInput] = useState('')
+
+    const handleChange = (e) => {
+        setInput(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: uuidv4(),
+            text: input
+        });
+        setInput('')
+    }
+    return (
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <input type="text"
+                   placeholder="Add a todo"
+                   value={input}
+                   name="text"
+                   className='todo-input'
+                   onChange={handleChange}/>
+            <button className='todo-button'>Add todo</button>
+        </form>
+    )
+}
+
+export default TodoForm;
